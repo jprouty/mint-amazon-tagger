56c6cc2965f1683dded00cfe1ad186418a599230
Jeff Prouty <jeff.prouty@gmail.com>
2020-03-22 15:18:24 -0700

    Releasing is fun

--- fbs_release.sh
+++ fbs_release.sh
@@ -9,10 +9,17 @@ source release_venv/bin/activate
 pip install --upgrade pip
 pip install --upgrade -r requirements/base.txt
 
+echo "Clean it"
+fbs clean
+
 echo "Open the app: verify it works"
 fbs run
 
 echo "Now make and upload the release"
 fbs freeze
+
+echo "Now verify the built version works"
+target/MintAmazonTagger.app/Contents/MacOS/MintAmazonTagger
+
 fbs installer
 fbs upload
