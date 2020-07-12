4c93ec50969fa5c3d52b4f76aef5797af8799b2f
Jeff Prouty <jeff.prouty@gmail.com>
2020-07-12 01:39:25 -0700

    macOS release script is all good now.

---
+++ release/dmg.applescript
@@ -0,0 +1,70 @@
+on run (volumeName)
+	tell application "Finder"
+		tell disk (volumeName as string)
+			open
+
+			set theXOrigin to 10
+			set theYOrigin to 60
+			set theWidth to 500
+			set theHeight to 350
+
+			set theBottomRightX to (theXOrigin + theWidth)
+			set theBottomRightY to (theYOrigin + theHeight)
+			set dsStore to "\"" & "/Volumes/" & volumeName & "/" & ".DS_STORE\""
+
+			tell container window
+				set current view to icon view
+				set toolbar visible to false
+				set statusbar visible to false
+				set the bounds to {theXOrigin, theYOrigin, theBottomRightX, theBottomRightY}
+				set statusbar visible to false
+			end tell
+
+			set opts to the icon view options of container window
+			tell opts
+				set icon size to 128
+				set text size to 16
+				set arrangement to not arranged
+			end tell
+
+			-- Positioning
+      set position of item "MintAmazonTagger.app" to {0, 10}
+
+			-- Hiding
+
+			-- Application and QL Link Clauses
+			set position of item "Applications" to {170, 10}
+			close
+			open
+			-- Force saving of the size
+			delay 1
+
+			tell container window
+				set statusbar visible to false
+				set the bounds to {theXOrigin, theYOrigin, theBottomRightX - 10, theBottomRightY - 10}
+			end tell
+		end tell
+
+		delay 1
+
+		tell disk (volumeName as string)
+			tell container window
+				set statusbar visible to false
+				set the bounds to {theXOrigin, theYOrigin, theBottomRightX, theBottomRightY}
+			end tell
+		end tell
+
+		--give the finder some time to write the .DS_Store file
+		delay 3
+
+		set waitTime to 0
+		set ejectMe to false
+		repeat while ejectMe is false
+			delay 1
+			set waitTime to waitTime + 1
+
+			if (do shell script "[ -f " & dsStore & " ]; echo $?") = "0" then set ejectMe to true
+		end repeat
+		log "waited " & waitTime & " seconds for .DS_STORE to be created."
+	end tell
+end run
--- release/macOS.sh
+++ release/macOS.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# "mac_bundle_identifier": "com.jeffprouty.mintamazontagger"
-# "app_name": "MintAmazonTagger",
-# "author": "Jeff Prouty",
-# "main_module": "src/main/python/mintamazontagger/main.py",
-# "version": "1.0.6",
-# "gpg_key": "CB1608BF9A09BE99908045E6E93C791A0BFE386F",
-# "gpg_name": "Jeff Prouty",
-# "url": "https://github.com/jprouty/mint-amazon-tagger"
-
 # exit when any command fails
 set -e
+
+readonly app_name="MintAmazonTagger"
+readonly bundle_ident="com.jeffprouty.mintamazontagger"
+readonly app_identity="Developer ID Application: Jeff Prouty (NRC455QXS5)"
+readonly email="jeff.prouty@gmail.com"
+
+readonly app_dir="dist/MintAmazonTagger.app"
+readonly dmg_path="dist/${app_name}.dmg"
+readonly entitlements="release/entitlements.plist"
+readonly icon_icns="build/Icon.icns"
 
 cd "$(dirname "$0")/.."
 
@@ -18,45 +19,91 @@ echo "Clean everything"
 python3 setup.py clean
 
 echo "Setup the release venv"
-python -m venv release_venv
+python3 -m venv release_venv
 source release_venv/bin/activate
 pip install --upgrade pip
 pip install --upgrade -r requirements/base.txt
 
-# https://github.com/pypa/setuptools/issues/1963
-# ?? --hidden-import='pkg_resources.py2_warn' \
+mkdir build
+
+# Create an icns file
+iconutil -c icns icons/mac.iconset --output="${icon_icns}"
+
 pyinstaller \
-  --name="MintAmazonTagger" \
+  --name="${app_name}" \
   --windowed \
-  --onefile \
+  --icon="${icon_icns}" \
+  --osx-bundle-identifier="${bundle_ident}" \
   mintamazontagger/main.py
 
 deactivate
 rm -rf release_venv
 
 echo "Signing the app"
-APP_IDENTITY="Developer ID Application: Jeff Prouty (NRC455QXS5)"
-EMAIL="jeff.prouty@gmail.com"
+codesign --verify --verbose --force --deep --sign \
+  "${app_identity}" \
+  --entitlements "${entitlements}" \
+  --options=runtime \
+  "${app_dir}"
 
-codesign --verify --verbose --force --deep --sign "${APP_IDENTITY}" --entitlements entitlements.plist --options=runtime target/MintAmazonTagger.app
+echo "Creating installer/disk image"
+readonly temp_dmg="dist/${app_name}.temp.dmg"
+hdiutil create \
+  -srcfolder "${app_dir}" \
+  -volname "${app_name}" \
+  -fs HFS+ \
+  -fsargs "-c c=64,a=16,e=16" \
+  -format UDRW \
+  "${temp_dmg}"
 
-echo "Creating installer"
-## TODO
+readonly mount_path="/Volumes/${app_name}"
+dev_name=$(hdiutil info | egrep --color=never '^/dev/' | sed 1q | awk '{print $1}')
+test -d "${mount_path}" && hdiutil detach "${dev_name}"
 
-# echo "Signing installer"
-# codesign --verify --verbose --force --deep --sign "${APP_IDENTITY}" --entitlements entitlements.plist --options=runtime target/MintAmazonTagger.dmg
+# Mount the image as RW.
+dev_name=$(hdiutil attach -readwrite -noverify -noautoopen "${temp_dmg}" | egrep --color=never '^/dev/' | sed 1q | awk '{print $1}')
+# Link in the apps dir
+ln -s /Applications "$mount_path/Applications"
+# Copy the icon, for fun.
+cp "${icon_icns}" "$mount_path/.VolumeIcon.icns"
+SetFile -c icnC "$mount_path/.VolumeIcon.icns"
+
+# Run the thing
+"/usr/bin/osascript" "release/dmg.applescript" "${app_name}" || true
+sleep 2
+
+chmod -Rf go-w "${mount_path}" &> /dev/null || true
+
+bless --folder "${mount_path}" --openfolder "${mount_path}"
+
+# tell the volume that it has a special file attribute for the icons.
+SetFile -a C "${mount_path}"
+
+hdiutil detach "${dev_name}"
+hdiutil convert "${temp_dmg}" \
+  -format "UDZO" \
+  -imagekey zlib-level=9 \
+  -o "${dmg_path}"
+rm -f "${temp_dmg}"
+
+echo "Signing dmg"
+codesign --verify --verbose --force --deep --sign \
+  "${app_identity}" \
+  --entitlements "${entitlements}" \
+  --options=runtime \
+  "${dmg_path}"
 
 echo "Creating an Apple notary request"
-# xcrun altool --notarize-app \
-#     --primary-bundle-id "com.jeffprouty.mintamazontagger" \
-#     --username "${EMAIL}" \
-#     --password "@keychain:AC_PASSWORD" \
-#     --file target/MintAmazonTagger.dmg
+xcrun altool --notarize-app \
+    --primary-bundle-id "${bundle_ident}" \
+    --username "${email}" \
+    --password "@keychain:AC_PASSWORD" \
+    --file "${dmg_path}"
 
 echo "Check notary status later via: "
-echo "xcrun altool --notarization-history 0 -u \"${EMAIL}\" -p \"@keychain:AC_PASSWORD\""
+echo "xcrun altool --notarization-history 0 -u \"${email}\" -p \"@keychain:AC_PASSWORD\""
 echo "  AND"
-echo "xcrun altool --notarization-info <REQUEST_ID> -u \"${EMAIL}\""
+echo "xcrun altool --notarization-info <REQUEST_ID> -u \"${email}\""
 echo
-echo "Once successful, staple and re-upload:"
-echo "xcrun stapler staple \"target/MintAmazonTagger.dmg\" && fbs upload"
+echo "Once successful, staple and you're done!:"
+echo "xcrun stapler staple \"${dmg_path}\"
