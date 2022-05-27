ac919abfd94c1e0f554099d38f41e815562ce994
Jeff Prouty <jeff.prouty@gmail.com>
2022-05-26 23:22:23 -0700

    Remove 'python3' from ubuntu scripts.

--- release/pypi.sh
+++ release/pypi.sh
@@ -6,13 +6,13 @@ set -e
 cd "$(dirname "$0")/.."
 
 # python3 setup.py block_on_version |
-python3 setup.py block_on_version clean sdist bdist_wheel || exit
+python setup.py block_on_version clean sdist bdist_wheel || exit
 
 # Publish to max_days_between_payment_and_shipping.
-python3 -m twine upload dist/*
+python -m twine upload dist/*
 
 # Verify the package is installable in a virtual env.
-python3 -m venv pypi_test_venv
+python -m venv pypi_test_venv
 source pypi_test_venv/bin/activate
 
 pip install --upgrade pip
