a4aeb13f9ba26293cf01392210fa5148332fb489
Jeff Prouty <jeff.prouty@gmail.com>
2022-05-26 23:21:08 -0700

    Remove 'python3' from ubuntu scripts.

--- release/ubuntu.sh
+++ release/ubuntu.sh
@@ -17,10 +17,10 @@ cd "$(dirname "$0")/.."
 # "author_email": "jeff.prouty@gmail.com",
 
 echo "Clean everything"
-python3 setup.py clean
+python setup.py clean
 
 echo "Setup the release venv"
-python3 -m venv release_venv
+python -m venv release_venv
 source release_venv/bin/activate
 pip install --upgrade pip
 pip install --upgrade -r requirements/base.txt -r requirements/ubuntu.txt
