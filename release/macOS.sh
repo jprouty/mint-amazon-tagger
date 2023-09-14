b07ddd17f301ee17323487c91a8ad7fc78ccbfe0
Jeff Prouty <jeff.prouty@gmail.com>
2023-09-14 16:04:23 -0700

    Fix cli arg.

--- release/macOS.sh
+++ release/macOS.sh
@@ -34,7 +34,7 @@ pyinstaller \
   --name="${app_name}" \
   --icon="${icon_icns}" \
   --osx-bundle-identifier="${bundle_ident}" \
-  --target_arch="universal2" \
+  --target-arch="universal2" \
   mintamazontagger/main.py
 
 deactivate
